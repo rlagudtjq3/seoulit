977977085b2aa4bb10fea709214e003dedad5798
김형섭 <@Mickey>
2016-08-22 20:32:22 +0900

    123141515

--- oov/src/oov/gooooooood.java
+++ oov/src/oov/gooooooood.java
@@ -4,7 +4,7 @@ public class gooooooood {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("¿ø°İÀúÀå¼Ò ¼º°ø????");
+			System.out.println("ë°˜ê°‘ìŠµë‹ˆë‹¤");
 	}
 
 }
